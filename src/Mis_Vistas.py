4c6b7b974293eff5a2b54fdbcde75c121e4d5e5a
Julián Perez <66093948+ositoMalvado@users.noreply.github.com>
2025-03-04 21:27:39 -0300

    probando arreglar

--- src/Mis_Vistas.py
+++ src/Mis_Vistas.py
@@ -1,94 +1,173 @@
 import flet as ft
 import mis_controles as mc
 
-#VP = Vista principal
-#VS = Vista secundaria
-#VS = Vista terciaria
+# VP = Vista principal
+# VS = Vista secundaria
+# VS = Vista terciaria
+
 
 class VP_Registrar(ft.View):
-    def __init__(self, navegationBar, page:ft.Page):
+    def __init__(self, navegationBar, page: ft.Page):
         super().__init__()
         self.route = "/registrar"
+
+        self.appbar = ft.AppBar(
+            leading_width=30,
+            title=ft.Text("Registrar", size=20, color="#27C8B2"),
+            bgcolor="#23182E",
+        )
         self.controls = [
-            ft.AppBar(),
-            ft.TextButton("Ir", on_click=lambda e: page.go("/registrar/biceps")),
-            mc.MyBotonR("Biceps", page),
-            mc.MyBotonR("Pecho", page),
-            mc.MyBotonR("Triceps", page),
-            mc.MyBotonR("Espalda", page),
-            mc.MyBotonR("Hombros", page),
-            mc.MyBotonR("Piernas", page),
+            ft.SafeArea(
+                expand=True,
+                content=ft.Column(
+                    expand=True,
+                    controls=[
+                        # ft.TextButton(
+                        #     "Ir", on_click=lambda e: page.go("/registrar/biceps")
+                        # ),
+                        mc.MyBotonR("Biceps", page),
+                        mc.MyBotonR("Pecho", page),
+                        mc.MyBotonR("Triceps", page),
+                        mc.MyBotonR("Espalda", page),
+                        mc.MyBotonR("Hombros", page),
+                        mc.MyBotonR("Piernas", page),
+                    ],
+                    scroll="auto",
+                ),
+            )
         ]
         self.navigation_bar = navegationBar
         self.bgcolor = "#CEC0A3"
-    
+
+
 class VS_Registrar_Opciones(ft.View):
     def __init__(self, navegationBar, page: ft.Page, diccionario):
         super().__init__()
         self.route = page.route
         self.navigation_bar = navegationBar
-
+        self.appbar = ft.AppBar(
+            leading_width=30,
+            title=mc.Texto_Secundario(page.route[11:].capitalize(), 20, "#27C8B2"),
+            bgcolor="#23182E",
+        )
         self.controls = [
-            ft.AppBar(leading_width= 30, title= mc.Texto_Secundario(page.route[11:].capitalize(), 20, "#27C8B2"), bgcolor="#23182E"),
-            ft.Column(
-                [mc.Selector_Principal("Ejercicio", page, diccionario), mc.Selector("Variacion", page)],
-                width= page.width,
-                horizontal_alignment= ft.CrossAxisAlignment.CENTER
-            ),
-            ft.Column(
-                [
-                    mc.Series(page),
-                    ft.Column([
-                            ft.Row([
-                                mc.Input("Reps", page),
-                                mc.Input("Kg", page)
-                            ], visible= False),
-                            ft.Row([
-                                mc.Input("Reps", page),
-                                mc.Input("Kg", page)
-                            ], visible= False),
-                            ft.Row([
-                                mc.Input("Reps", page),
-                                mc.Input("Kg", page)
-                            ], visible= False),
-                            ft.Row([
-                                mc.Input("Reps", page),
-                                mc.Input("Kg", page)
-                            ], visible= False),
-                            ft.Row([
-                                mc.Input("Reps", page),
-                                mc.Input("Kg", page)
-                            ], visible= False),
-                            ft.Row([
-                                mc.Input("Reps", page),
-                                mc.Input("Kg", page)
-                            ], visible= False),
-                    ]),
-                    ft.Column(height= 25),
-                    mc.Boton_Enviar(page)
-                ],
-                width= page.width,
-                horizontal_alignment= ft.CrossAxisAlignment.CENTER
+            ft.SafeArea(
+                expand=True,
+                content=ft.Column(
+                    expand=True,
+                    controls=[
+                        ft.Column(
+                            [
+                                mc.Selector_Principal("Ejercicio", page, diccionario),
+                                mc.Selector("Variacion", page),
+                            ],
+                            width=page.width,
+                            horizontal_alignment=ft.CrossAxisAlignment.CENTER,
+                        ),
+                        ft.Column(
+                            [
+                                mc.Series(page),
+                                ft.Column(
+                                    [
+                                        ft.Row(
+                                            [
+                                                mc.Input("Reps", page),
+                                                mc.Input("Kg", page),
+                                            ],
+                                            visible=False,
+                                        ),
+                                        ft.Row(
+                                            [
+                                                mc.Input("Reps", page),
+                                                mc.Input("Kg", page),
+                                            ],
+                                            visible=False,
+                                        ),
+                                        ft.Row(
+                                            [
+                                                mc.Input("Reps", page),
+                                                mc.Input("Kg", page),
+                                            ],
+                                            visible=False,
+                                        ),
+                                        ft.Row(
+                                            [
+                                                mc.Input("Reps", page),
+                                                mc.Input("Kg", page),
+                                            ],
+                                            visible=False,
+                                        ),
+                                        ft.Row(
+                                            [
+                                                mc.Input("Reps", page),
+                                                mc.Input("Kg", page),
+                                            ],
+                                            visible=False,
+                                        ),
+                                        ft.Row(
+                                            [
+                                                mc.Input("Reps", page),
+                                                mc.Input("Kg", page),
+                                            ],
+                                            visible=False,
+                                        ),
+                                    ]
+                                ),
+                                ft.Column(height=25),
+                                mc.Boton_Enviar(page),
+                            ],
+                            width=page.width,
+                            horizontal_alignment=ft.CrossAxisAlignment.CENTER,
+                        ),
+                    ],
+                ),
             )
         ]
         self.bgcolor = "#CEC0A3"
+
 
 class VP_Entrenar(ft.View):
     def __init__(self, navegationBar):
         super().__init__()
         self.route = "/entrenamiento"
+        self.appbar = ft.AppBar(
+            leading_width=30,
+            title=ft.Text("Entrenamiento", size=20, color="#27C8B2"),
+            bgcolor="#23182E",
+        )
         self.controls = [
-            ft.Column([ft.Text("")], height= 30), #Espacio en blanco
-            ft.Text("Entrenamiento", color="Blue")
+            ft.SafeArea(
+                expand=True,
+                content=ft.Column(
+                    expand=True,
+                    controls=[
+                        ft.Column([ft.Text("")], height=30),  # Espacio en blanco
+                    ],
+                ),
+            )
         ]
         self.navigation_bar = navegationBar
+
 
 class VP_Analizar(ft.View):
     def __init__(self, navegationBar):
         super().__init__()
         self.route = "/analisis"
+
+        self.appbar = ft.AppBar(
+            leading_width=30,
+            title=ft.Text("Análisis", size=20, color="#27C8B2"),
+            bgcolor="#23182E",
+        )
         self.controls = [
-            ft.Column([ft.Text("")], height= 30), #Espacio en blanco
-            ft.Text("Analisis", color="Blue")
+            ft.SafeArea(
+                expand=True,
+                content=ft.Column(
+                    expand=True,
+                    controls=[
+                        ft.Column([ft.Text("")], height=30),  # Espacio en blanco
+                    ],
+                ),
+            )
         ]
         self.navigation_bar = navegationBar
